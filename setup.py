ba9a27f442ff3ad6074efcf4f76a2bc5498e4791
Matthew King <automatthew@gmail.com>
2014-06-03 12:30:07 -0500

    initial setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='coinop',
+      version='0.0.1',
+      description='Crypto-currency conveniences',
+      url='http://github.com/BitVault/coinop-py',
+      author='Matthew King',
+      author_email='matthew@pandastrike.com',
+      license='MIT',
+      packages=['coinop'],
+      install_requires=[
+      ],
+      zip_safe=False)
+
